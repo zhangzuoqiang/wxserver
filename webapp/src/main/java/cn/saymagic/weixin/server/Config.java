c61a296e270f8a4f1dd21814863309a942bad668
zhangzuoqiang <zhangzuoqiang@aliyun.com>
2015-09-06 13:45:52 +0800

    update

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -3,9 +3,9 @@ package cn.saymagic.weixin.server;
 public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
-	public static final String TOKEN = "";
+	public static final String TOKEN = "token4wxserver";
 	
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
-	public static final String APIKEY = ""; 
+	public static final String APIKEY = "c9c17dc8ef616ad90f188b7d6cb129ef";
 	
 }
